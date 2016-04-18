bbdf6ab58c54480b67840af9ce51420c73e2dd66
ABeardedSpecialist <lisauskas.tadaS@gmail.com>
2016-04-18 15:26:36 +0200

    A branch for docs only.

--- DatabaseDump/webshop_products.sql
+++ DatabaseDump/webshop_products.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.11, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
--- Host: localhost    Database: webshop
+-- Host: 127.0.0.1    Database: webshop
 -- ------------------------------------------------------
--- Server version	5.7.11-log
+-- Server version	5.7.12-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -32,7 +32,7 @@ CREATE TABLE `products` (
   `ProductCategory` varchar(45) NOT NULL,
   `ProductSubcategory` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`ProductID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +41,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (2,'Acer Preditor XH2345',8000,3,'/images/hej.jpg','hej','Monitor','Acer'),(3,'MSI 980 ti',5000,5,' ','bla bla','graphic card','MSI'),(4,'Peter',100,9,' ','bla','kuk','penis');
+INSERT INTO `products` VALUES (2,'Acer Preditor XH2345',8000,3,'/images/hej.jpg','hej','Monitor','Acer'),(3,'MSI 980 ti',5000,5,' ','bla bla','graphic card','MSI'),(4,'Peter',100,9,' ','bla','kuk','penis'),(5,'Samsung ATIV Book 9',15000,1,'http://www.samsung.com/us/system/consumer/product/np/93/0x/np930x2kk01us/NewBook9_01.jpg','The Samsung ATIV Book 9 connects you to your work and entertainment on the go with a hyper-real display and studio-quality audio, all packed into an impressively thin, lightweight design.','Laptops','12\"');
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-04-16 16:15:11
+-- Dump completed on 2016-04-18 15:18:50
