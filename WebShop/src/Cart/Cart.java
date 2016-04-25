ca862a2122f2ff23658d6f39df16ca64afa1b42f
xoften <mj223gn@student.lnu.se>
2016-04-25 14:47:56 +0200

    cart is almost done

---
+++ WebShop/src/Cart/Cart.java
@@ -0,0 +1,76 @@
+package Cart;
+
+import Products.product;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Michael Johansson(mj223gn) on 2016-04-22.
+ */
+@Named
+@SessionScoped
+public class Cart implements Serializable {
+
+    private static final String sql_connection = "jdbc:mysql://localhost:3306/webshop";;
+    private List<cartItem> ID = new ArrayList<>();
+    private int totalPrice;
+    private int numberOfProducts;
+
+    public int getTotalPrice() {
+        totalPrice = 0;
+        for (cartItem ci:ID) {
+            totalPrice += (ci.getQuantity()*ci.getItem().getProductPrice());
+        }
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getNumberOfProducts() {
+        numberOfProducts = 0;
+        for (cartItem ci:ID) {
+            numberOfProducts += ci.getQuantity();
+        }
+        return numberOfProducts;
+    }
+
+    public void setNumberOfProducts(int numberOfProducts) {
+        this.numberOfProducts = numberOfProducts;
+    }
+    public void removeProductFromCart(cartItem ci) {
+        ID.remove(ID.indexOf(ci));
+    }
+
+
+
+    public List<Integer> getQuantity(int quantity){
+        List<Integer> Quantity = new ArrayList<>();
+        for (int i = 1; i <= quantity; i++){
+            Quantity.add(i);
+        }
+        return Quantity;
+    }
+    public void addProductToCart(product prod){
+        for (cartItem ci: ID) {
+            if(ci.getItem().getProductID() == prod.getProductID()){
+                ci.setQuantity(+1);
+                return;
+            }
+        }
+        cartItem ci = new cartItem();
+        ci.setQuantity(1);
+        ci.setItem(prod);
+        ID.add(ci);
+    }
+    public List<cartItem> getID() {
+        return ID;
+    }
+}
